cd295aa437439b4fac388600653faa8fb8eb19d2
katoltmans <katoltmans@msn.com>
2022-08-08 20:22:07 -0400

    Add theme for use with breakpoints for mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
+import { ThemeProvider, useTheme } from "@emotion/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Welcome from "./components/Welcome";
 import PortfolioView from "./views/PortfolioView";
 
 function App() {
+    const theme = useTheme();
+
     return (
         <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Welcome />} default />
-                    <Route path="/portfolio" element={<PortfolioView />} />
-                </Routes>
-            </div>
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Welcome />} default />
+                        <Route path="/portfolio" element={<PortfolioView />} />
+                    </Routes>
+                </div>
+            </ThemeProvider>
         </BrowserRouter>
     );
 }
